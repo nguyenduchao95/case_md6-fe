6f22bcb0d37e5a03c4e6989be0774d60c5bff756
hieu16129 <infor.duchieu.1999@gmail.com>
2023-10-04 17:33:43 +0700

    validate pwd

--- src/component/account/crud/ChangePassword.js
+++ src/component/account/crud/ChangePassword.js
@@ -1,24 +1,28 @@
 import React, {useEffect, useState} from 'react';
-import {Field, Form, Formik} from "formik";
-
+import {ErrorMessage, Field, Form, Formik} from "formik";
 import * as Yup from "yup";
 import {useParams} from "react-router-dom";
 import {toast} from 'react-toastify';
 import accountService from "../AccountService";
-import async from "async";
 const ChangePassword = () => {
-/*    const validateSchema = Yup.object().shape({
+
+    const blankRegex = /[\s]/
+    const validateSchema = Yup.object().shape({
         newPassword: Yup.string()
             .min(6, "Mật khẩu có độ dài 6-18 ký tự!")
-            .max(18 , "Mật khẩu có độ dài 6-18 ký tự!")
-            .required("Mật khẩu không được để trống"),
+            .max(18, "Mật khẩu có độ dài 6-18 ký tự!")
+            .required("Mật khẩu không được để trống")
+            .test('no-whitespance', "Mật không để trống hoặc chứa dấu cách", function (value) {
+                return !blankRegex.test(value);
+            }),
         confirmNewPassword: Yup.string()
             .min(6, "Mật khẩu có độ dài 6-18 ký tự!")
-            .max(18 , "Mật khẩu có độ dài 6-18 ký tự!")
-            .required("Mật khẩu không được để trống"),
-
-    });*/
-
+            .max(18, "Mật khẩu có độ dài 6-18 ký tự!")
+            .required("Mật khẩu không được để trống")
+            .test('no-whitespance', "Mật không để trống hoặc chứa dấu cách", function (value) {
+                return !blankRegex.test(value);
+            })
+    });
     const {id} = useParams();
     const [account, setAccount] = useState({});
 
@@ -32,35 +36,30 @@ const ChangePassword = () => {
             console.log(err);
         })
     }
-
     const handleChangePassword = (values) => {
-      /*  if (account.password !== values.password) {
-            toast.error("Mật khẩu hiện tại không đúng!!!", {position: "top-center", autoClose: 1000,});
-        }else {
-            if (values.newPassword !== values.confirmNewPassword){
-                toast.error("Mật khẩu mới không đúng!!!", {position: "top-center", autoClose: 1000,});
-            }else {
-                setAccount({...account , password : values.newPassword});
-                console.log()
-                console.log(account);
-              /!*  editPassword();*!/
-            }
-        }*/
-        console.log(account);
+
         accountService.checkPassword(account).then((response) => {
-            alert("ok");
+            if (!response) {
+                toast.error("Mật khẩu hiện tại không đúng!!!", {position: "top-center", autoClose: 1000,});
+            } else {
+                if (values.newPassword !== values.confirmNewPassword) {
+                    toast.error("Mật khẩu mới không đúng!!!", {position: "top-center", autoClose: 1000,});
+                } else {
+                    const newAccount = {...account, password: values.newPassword};
+                    editPassword(newAccount);
+                }
+            }
         }).catch(function (err) {
             console.log(err);
+
         })
     }
-
-    /*  const editPassword = () => {
-          console.log(account);
-          accountService.changePassWord(account).then((response) => {
-              toast.success("Thay đổi mật khẩu thành công", {position: "top-center", autoClose: 1000,});
-          });
-    }*/
-
+    const editPassword = (account) => {
+        console.log(account);
+        accountService.changePassWord(account).then((response) => {
+            toast.success("Thay đổi mật khẩu thành công", {position: "top-center", autoClose: 1000,});
+        });
+    }
     return (
         <div className={"container"}>
 
@@ -73,49 +72,58 @@ const ChangePassword = () => {
 
                                     <h3 className="mb-5" style={{color: "#0174c3"}}>Thay đổi mật khẩu</h3>
                                     <Formik initialValues={{
-                                        password : '',
-                                        newPassword : '',
-                                        confirmNewPassword : ''
+                                        password: '',
+                                        newPassword: '',
+                                        confirmNewPassword: ''
                                     }}
-                                         //   validationSchema={validateSchema}
+                                            validationSchema={validateSchema}
                                             onSubmit={(values) => {
                                                 handleChangePassword(values);
                                             }}>
 
-                                    <Form>
-                                        <div className="form-outline mb-4">
-                                            <Field type="text" id="password" name="password"
-                                                   className="form-control form-control-lg"
-                                              onInput = {(event) => {
-                                                setAccount({...account , password : event.target.value})
-                                              }} />
-                                            <div >
-                                                <label className="form-label" htmlFor="password">Mật khẩu hiện tại</label>
+                                        <Form>
+                                            <div className="form-outline mb-4">
+                                                <Field type="text" id="password" name="password"
+                                                       className="form-control form-control-lg"
+                                                       onInput={(event) => {
+                                                           setAccount({...account, password: event.target.value})
+                                                       }}/>
+                                                <div>
+                                                    <label className="form-label" htmlFor="password">Mật khẩu hiện
+                                                        tại</label>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className="form-outline mb-4">
-                                            <Field type="password" id="newPassword" name ="newPassword"
-                                                   className="form-control form-control-lg"
-                                                   />
-                                            <div >
-                                                <label className="form-label" htmlFor="newPassword">Mật khẩu mới </label>
+                                            <div className="form-outline mb-4">
+                                                <Field type="password" id="newPassword" name="newPassword"
+                                                       className="form-control form-control-lg"
+                                                />
+                                                <div>
+                                                    <label className="form-label" htmlFor="newPassword">Mật khẩu
+                                                        mới </label>
+                                                </div>
+                                                <span style={{color: 'red'}}>
+                                        <ErrorMessage name={'newPassword'}></ErrorMessage>
+                                   </span>
                                             </div>
-                                        </div>
-                                        <div className="form-outline mb-4">
-                                            <Field type="password" id="confirmNewPassword" name="confirmNewPassword"
-                                                   className="form-control form-control-lg"
-                                                   />
-                                            <div>
-                                                <label className="form-label" htmlFor="confirmNewPassword">Xác nhận mật khẩu mới</label>
+                                            <div className="form-outline mb-4">
+                                                <Field type="password" id="confirmNewPassword" name="confirmNewPassword"
+                                                       className="form-control form-control-lg"
+                                                />
+                                                <div>
+                                                    <label className="form-label" htmlFor="confirmNewPassword">Xác nhận
+                                                        mật khẩu mới</label>
+                                                </div>
+                                                <span style={{color: 'red'}}>
+                                        <ErrorMessage name={'confirmNewPassword'}></ErrorMessage>
+                                   </span>
                                             </div>
-                                        </div>
-                                        <hr className="my-4"/>
-                                        <button className="btn btn-lg btn-block btn-primary"
-                                                style={{backgroundColor: "#39dd86 ", color: "black"}}
-                                                type="submit">
-                                            Xác nhận thay đổi
-                                        </button>
-                                    </Form>
+                                            <hr className="my-4"/>
+                                            <button className="btn btn-lg btn-block btn-primary"
+                                                    style={{backgroundColor: "#39dd86 ", color: "black"}}
+                                                    type="submit">
+                                                Xác nhận thay đổi
+                                            </button>
+                                        </Form>
 
                                     </Formik>
                                 </div>
@@ -128,5 +136,4 @@ const ChangePassword = () => {
         </div>
     );
 };
-
 export default ChangePassword;
