6a2e9d1b84928522b4abdac82d67e8e2f9e126b0
hieu16129 <infor.duchieu.1999@gmail.com>
2023-10-16 23:57:21 +0700

    search rental history

--- src/components/Profile/RentalHistory.js
+++ src/components/Profile/RentalHistory.js
@@ -5,6 +5,7 @@ import BookingService from "../../service/BookingService";
 import {convertDateFormat, formatCurrency} from "../../service/format";
 import {Pagination} from "@mui/material";
 import Swal from "sweetalert2";
+import SearchBooking from "./searchBooking/SearchBooking";
 
 const RentalHistory = () => {
     const account = useSelector(state => state.account);
@@ -12,28 +13,75 @@ const RentalHistory = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
     const [load, setLoad] = useState(false);
+    const [houseName , setHouseName] = useState('');
+    const [status , setStatus] = useState('');
+    const [startTime , setStartTime] = useState(null);
+    const [endTime , setEndTime] = useState(null);
+    const [localStartTime , setLocalStartTime] = useState(null);
+    const [localEndTime , setLocalEndTime] = useState(null);
+
     useEffect(() => {
+
         getRentalList(account.id, currentPage - 1);
-    }, [currentPage])
+    }, [currentPage , houseName , status , localStartTime , localEndTime])
 
     useEffect(() => {
         getRentalList(account.id, currentPage - 1);
         setLoad(false);
     }, [load])
+
+    const handleHouseName = (e) => {
+        let {value} = e.target;
+        console.log(value);
+        setHouseName(value);
+    }
+
+    const changeDate = (dayValue) => {
+        const date = dayValue.split("-");
+        const year = parseInt(date[0]);
+        const month = parseInt(date[1]);
+        const day = parseInt(date[2]);
+
+        if (!isNaN(year) && !isNaN(month) && !isNaN(day)) {
+            const localDateTime = new Date(year, month - 1, day);
+            localDateTime.setMinutes(0);
+            localDateTime.setSeconds(0);
+
+            return localDateTime.toISOString().slice(0, 16);
+        } else {
+            return "";
+        }
+    }
+    const handleStartTime = (e) => {
+        console.log();
+        setStartTime(e.target.value);
+        setLocalStartTime(changeDate(e.target.value));
+    }
+
+    const handleEndTime = (e) => {
+        console.log(e.target.value);
+        setEndTime(e.target.value);
+        setLocalEndTime(changeDate(e.target.value))
+    }
+
     const getRentalList = (id, currentPage) => {
-        BookingService.getHistoryByAccount(id, currentPage).then((response) => {
+        let booking = {
+            houseName : houseName,
+            status : status,
+            startTime : localStartTime ,
+            endTime : localEndTime
+        };
+        BookingService.getHistoryByAccount(id, currentPage , booking).then((response) => {
             const result = response.data.content;
             console.log(result)
             setRentalList(result);
             setTotalPages(response.data.totalPages);
         }).catch(function (err) {
             console.log(err);
-        })
-    }
+        })}
     const changePage = (e, value) => {
         setCurrentPage(value);
     }
-
     const cancelBooking = (item) => {
         BookingService.cancelBooking(item.id).then((response) => {
             Swal.fire({
@@ -47,7 +95,6 @@ const RentalHistory = () => {
             console.log(err);
         })
     }
-
     const showCancelBookingConfirm = (booking) => {
         if (new Date(booking.startTime) - new Date() > (1000 * 60 * 60 * 48)) {
             Swal.fire({
@@ -92,6 +139,40 @@ const RentalHistory = () => {
         <div className={'col-9'}>
             <div>
                 <h3 className="text-uppercase text-center mb-4">Lịch sử thuê nhà</h3>
+                <div className="mb-3 py-4 px-3"
+                     style={{backgroundColor: "rgb(0,185,142)"}}>
+                    <div className="row g-2">
+                        <div className="col-md-3">
+                            <select className="form-select py-2 border-0" value={status} onChange={(e)=>{
+                                setStatus(e.target.value)
+                            }}
+                                 style={{minWidth: '200px'}}>
+                                <option value="">Tất cả</option>
+                                <option value="Chờ nhận phòng">Chờ nhận phòng</option>
+                                <option value="Đã trả phòng">Đã trả phòng</option>
+                                <option value="Đã hủy">Đã hủy</option>
+                                <option value="Đang ở">Đang ở</option>
+                                <option value="Chờ xác nhận">Chờ xác nhận</option>
+                            </select>
+                        </div>
+
+                        <div className="col-md-5">
+                            <input type="text" className="form-control border-0 py-2" placeholder="Nhập từ khóa tìm kiếm"
+                                   onInput={handleHouseName} name="houseName" value={houseName}/>
+                        </div>
+                        <div className="col-2">
+                            <div className="input-group">
+                                <input type="date" className="form-control" name="startTime" onChange={handleStartTime}   value={startTime} />
+                            </div>
+                        </div>
+                        <div className="col-2">
+                            <div className="input-group">
+                                <input type="date" className="form-control" name="endTime" onChange={handleEndTime} value={endTime}  />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <table className="table">
                     <thead>
                     <tr align="center">
--- src/service/BookingService.js
+++ src/service/BookingService.js
@@ -1,8 +1,8 @@
 import instance from "./axiosConfig";
 
 const BookingService = {
-    getHistoryByAccount: async (id, currentPage = 0) => {
-        return await instance.get(`/api/bookings/getByIdAccount/${id}?page=${currentPage}&`);
+    getHistoryByAccount: async (id, currentPage = 0 , booking) => {
+        return await instance.post(`/api/bookings/getByIdAccount/${id}?page=${currentPage}` , booking);
     },
     cancelBooking: async (idBooking) => {
         return await instance.get("/api/bookings/cancelBooking/" + idBooking);
